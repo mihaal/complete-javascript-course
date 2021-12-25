8cac8ac38118a399545b3584d2982fb87be8184a
Michael Scholl <michaelscholl1@gmx.de>
2021-12-25 05:36:22 +0100

    Selektoren in JS auswählen & ihren Text manipulieren

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number! 🎉';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 11;
+
+console.log((document.querySelector('.guess').value = 23));
