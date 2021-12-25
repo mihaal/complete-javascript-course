14bdba5cb17e86f1c1a137d443ea82dc2df9e08d
Michael Scholl <michaelscholl1@gmx.de>
2021-12-25 05:54:09 +0100

    Error Handling if no guess

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,9 +1,17 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'Correct Number! 🎉';
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number! 🎉';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 11;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 11;
 
-console.log((document.querySelector('.guess').value = 23));
+// document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number! ⛔';
+  }
+});
