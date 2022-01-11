4c308973811e9ec61f84764e91aaf30205eb648b
Michael Scholl <michaelscholl1@gmx.de>
2022-01-11 10:07:29 +0100

    Modal fertig

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,59 @@
 'use strict';
+
+// Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting Conditions
+score1El.textContent = 0;
+score0El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling Dice
+btnRoll.addEventListener('click', function () {
+  // 1. Generate a random Diceroll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display The dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next Player
+  if (dice !== 1) {
+    // add dice to score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
+
+// Wenn nicht mit toggle dann so Player switchen
+
+// const switchPlayer = () => {
+//   document
+//     .querySelector(`.player--${activePlayer}`)
+//     .classList.remove('player--active');
+//   activePlayer = activePlayer === 0 ? 1 : 0;
+//   document
+//     .querySelector(`.player--${activePlayer}`)
+//     .classList.add('player--active');
+// };
